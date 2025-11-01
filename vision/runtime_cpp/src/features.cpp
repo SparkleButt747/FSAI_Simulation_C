617bac2e161ae29598ee2e7dcdda6b227e6177f0
Wai Hei CHAN <ricerice562@gmail.com>
2025-11-01 11:25:36 +0000

    Updated initial design, finished extract_boundimg, need to validate extract_boundimg

--- vision/runtime_cpp/src/features.cpp
+++ vision/runtime_cpp/src/features.cpp
@@ -1,36 +1,38 @@
 #include <iostream>
+#include <opencv2/opencv.hpp>
 #include <features.hpp> 
 #include <vector> 
 // high level overview 
 // we need function to take left and right frames and bounding box and return xy coordinates of matched features for each bounding box
 
+cv::Mat extract_boundimg(cv::Mat left_frame, BoxBound box_bound){
+    // extracts the bounding box image for a single bounding box object 
+    cv::Rect roi(box_bound.x, box_bound.y, box_bound.w, box_bound.h);   // define region of interest for extraction 
+    cv::Mat box_boundimg left_frame(roi);                               // extracts the region we want as a reference (we never modify it anyways, no need to clone)
+    return box_boundimg;                                                // returns a reference to the part of the frame we want 
 
-cv::Mat extract_boximg(cv::Mat left_frame, BoxBound box_bound){
-    // should be quite straight forward
-    // we extract the bounding box image from the left frame
-    // we return an opencv matrix whcih is a matrix of colour values 
 }
 
-cv::Mat project_right(cv::Mat left_boximg, cv::Mat right_frame){
-    // we apply pnp here 
-    // not sure how to implement this without having to do stereo triangulation...
-    // we take the opencv matrix from extractboximg and apply PnP 
-    // this projects the bounding box onto the right frame 
-    // maybe it could work by just taking the top left point, then using same width and height 
-    // maybe needs stereotriangulation???
+std::vector<pseudofeature> extract_feature(BoxBound box_bound){
+    // extract features using ORB or SIFT techniques 
+    // each pseudofeature has xy coordinates in the coordinate system of the frame image, and a descriptor matrix used for ORB or SIFT
 }
 
-std::vector<feature> bound_matching(cv::Mat left_boximg, cv::Mat right_boximg){
-    // we apply feature matching between bounding box 
-    // feature matching algorithm tbd 
-    // this extracts xy coordinates relative to top left of bounding box
-    // pass into fix_relativity to make it relative to left frame and right frame 
+std::vector<pseudofeature> extract_right_features(cv::Mat right_frame){
+    // extract all features from the right frame 
+    // each pseudofeature has xy coordinates in coordinate system of right frame, and a descriptor matrix used for ORB or SIFT 
 }
 
-std::vector<feature> fix_relativity(std::vector<feature> unfixed_features){
-    // we turn the features extracted from bounding boxes from relative to bounding box to relative to frame 
-    // should be quite straight forward  
+std::vector<std::tuple<pseudofeature>> pair_features(std::vector<feature> left_features, std::vector<feature> right_feature){
+    // use epipolar constraint to search along epipolar line for each left feature to find matching right feature 
+    // return an "array" of (left_feature, right_feature) to pass for coordinate calculation 
 }
+
+std::vector<feature> extract_coordinates(std::vector<std::tuple<feature>> pairs){
+    // we format the pseudofeatures into actual features which look like (x1,y1,x2,y2)
+    // x1y1 are coordinates in left frame and x2y2 are coordinates in right frame
+}
+
 
 std::vector<feature> match_features(cv::Mat left_frame, cv::Mat right_frame,std::vector<BoxBound> box_bounds){
     // loop over each bounding box
