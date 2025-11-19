cc3ee32fec6ec80a1b0a08053d6d6a07447cd679
Nikita Kroshko <nikita.kroshko@student.manchester.ac.uk>
2025-11-19 17:46:54 +0000

    fix asynch log cpp

--- vision/runtime_cpp/src/asynch_logger.cpp
+++ vision/runtime_cpp/src/asynch_logger.cpp
@@ -1,32 +1,35 @@
 #include "asynch_logger.hpp"
+#include <quill/Backend.h>
+#include <quill/Frontend.h>
+#include <quill/sinks/FileSink.h>
+#include <quill/LogMacros.h>
 
-namespace fsai:vision
+namespace fsai::vision
 {
-	Asynch_logger::Asynch_logger(const std::string& logger_name = "app_logger", const std::string& file_path = "app.log"):
-	{
-		quill::start();
-		logger_ = quill::get_logger(logger_name.c_str());
-		logger_->add_sink(quill::file_sink(file_path.c_str()));
-	}
+    Asynch_logger::Asynch_logger(const std::string& logger_name, const std::string& file_path)
+    {
+        quill::Backend::start();
+        auto sink = std::make_shared<quill::FileSink>(file_path.c_str());
+        logger_ = quill::Frontend::create_or_get_logger( logger_name.c_str(), { sink } );
+    }
 
-	Asynch_logger::info
-	{
-		LOG_INFO(logger_, "{}", msg);
-	}
+    void Asynch_logger::info(const std::string& msg)
+    {
+        LOG_INFO(logger_, "{}", msg);
+    }
 
-	Asynch_logger::warn
-	{
-		LOG_WARNING(logger_, "{}", msg);
-	}
+    void Asynch_logger::warn(const std::string& msg)
+    {
+        LOG_WARNING(logger_, "{}", msg);
+    }
 
-	Asynch_logger::error
-	{
-		LOG_ERROR(logger_, "{}", msg);
-	}
+    void Asynch_logger::error(const std::string& msg)
+    {
+        LOG_ERROR(logger_, "{}", msg);
+    }
 
-	Asynch_logger::debug
-	{
-		LOG_DEBUG(logger_, "{}", msg);
-	}
+    void Asynch_logger::debug(const std::string& msg)
+    {
+        LOG_DEBUG(logger_, "{}", msg);
+    }
 }
-
