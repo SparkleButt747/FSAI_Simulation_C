663d59ab7515099c4b1dd0426476ca84c22c58d4
Nikita Kroshko <nikita.kroshko@student.manchester.ac.uk>
2025-11-19 15:57:39 +0000

    create the asynch logger header

---
+++ vision/runtime_cpp/src/asynch_logger.hpp
@@ -0,0 +1,47 @@
+/**
+ * @brief Asynchronous logger
+ */
+
+#pragma once
+#include <string>
+#include <quill/Quill.h>
+
+namespace fsai::util {
+    class Asynch_logger{
+    public:
+        /**
+         * @brief Constructs a simple async logger.
+         * @param logger_name Name of the logger.
+         * @param file_path Path to the output log file.
+         */
+        explicit Asynch_logger(const std::string& logger_name = "app_logger",
+                               const std::string& file_path   = "app.log");
+
+        /**
+         * @brief Logs an informational message.
+         * @param msg string.
+         */
+	void info(const std::string& msg);
+
+        /**
+         * @brief Logs a warning message.
+         * @param msg string.
+         */
+	void warn(const std::string& msg);
+
+        /**
+         * @brief Logs an error message.
+         * @param msg string.
+         */
+	void error(const std::string& msg);
+
+        /**
+         * @brief Logs a debug message.
+         * @param msg string.
+         */
+	void debug(const std::string& msg);
+
+    private:
+        quill::Logger* logger_;
+    };
+}
