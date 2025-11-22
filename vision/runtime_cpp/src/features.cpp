2c0e069753d400c876b308c4a90e0c85652b1897
Wai Hei CHAN <ricerice562@gmail.com>
2025-11-22 13:32:07 +0000

    Fixed small feature matching bug, include changes

--- vision/runtime_cpp/src/features.cpp
+++ vision/runtime_cpp/src/features.cpp
@@ -1,4 +1,5 @@
 #include "features.hpp"
+#include "common/include/common/types.h"
 #include "detect.hpp"
 #include <vector>
 #include <opencv2/opencv.hpp>
@@ -37,6 +38,20 @@ std::vector<ConeMatches> match_features_per_cone(const cv::Mat& left_frame,
     std::vector<PseudoFeature> right_features;
     cv::Rect image_bounds(0, 0, left_frame.cols, left_frame.rows);
     for (int i = 0; i < box_bounds.size(); ++i){
+        // get information about the box 
+        fsai::types::BoxBound box_i = box_bounds[i]; 
+        int box_index = i; 
+        
+        // get roi 
+        cv::Rect box_rect(box_i.x, box_i.y, box_i.w, box_i.h); 
+        cv::Mat box_roi; 
+        if (!is_safe_roi(left_frame, box_rect)){continue;}
+        box_roi = left_frame(box_rect); 
+        std::vector<cv::KeyPoint> left_keypoints; 
+        cv::Mat left_descriptors; 
+        
+        sift_detector->detectAndCompute(box_roi, cv::noArray(), left_keypoints, left_descriptors); 
+        
         fsai::types::BoxBound box_i = box_bounds[i];
         int box_index = i;
 
@@ -146,8 +161,8 @@ std::vector<ConeMatches> match_features_per_cone(const cv::Mat& left_frame,
 
         tempConeMatch.cone_index = cone_index;
         tempConeMatch.bound = box_bounds[cone_index];
-        tempConeMatch.matches = pair.second;
-
+        tempConeMatch.matches = pair.second; 
+        
         all_cone_matches.push_back(tempConeMatch);
     }
     return all_cone_matches;
